e4b902e703371d33d831d202a3dfbd90db58b073
99002476 <71481159+99002476@users.noreply.github.com>
2020-10-19 13:54:25 +0530

    Create string.h

---
+++ dynamic_lib/inc/string.h
@@ -0,0 +1,11 @@
+
+#ifndef STRING_H_INC
+#define STRING_H_INC
+
+
+const char *concat(char* a1, char* a2);
+int mystrlen(char* a1);
+char *mystrcpy(char* a1);
+int *mystrcmp(char* a1,char* a2);
+
+#endif // STRING_H_INC
