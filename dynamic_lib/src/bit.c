e04c95bac053f18ce9394ba7e8678dd568dce8e4
99002476 <71481159+99002476@users.noreply.github.com>
2020-10-19 13:57:11 +0530

    Create bit.c

---
+++ dynamic_lib/src/bit.c
@@ -0,0 +1,25 @@
+#include"bit.h"
+// Function to set the bth bit of a 
+
+int setBit(int a, int b) 
+{ 
+    return (a | (1 << (b - 1))); 
+} 
+  
+// Function to clear the bth bit of a
+int clearBit(int a, int b) 
+{ 
+    return (a & (~(1 << (b - 1)))); 
+} 
+  
+// Function to toggle the bth bit of a 
+int toggleBit(int a, int b) 
+{ 
+    return (a ^ (1 << (b - 1))); 
+} 
+
+//Function to query
+int query(int a,int b)
+{
+    return(a & (1<<(b-1)));
+}
